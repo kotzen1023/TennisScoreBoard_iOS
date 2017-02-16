1a7b4fe38b9c87f721ce66f5a612c1e99629a0c0
SUNUP <sunup@sunupteki-mac-mini.local>
2017-02-16 12:07:41 +0800

    Initial Commit

---
+++ TennisScoreBoard_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TennisScoreBoard_iOS
+//
+//  Created by SUNUP on 2017/2/16.
+//  Copyright © 2017年 RichieShih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
