aef8fbbe247e2dff23b8eb4a00bf049fb6de7c18
SUNUP <sunup@sunupteki-mac-mini.local>
2017-03-13 17:17:20 +0800

    modify

--- TennisScoreBoard_iOS/ViewController.swift
+++ TennisScoreBoard_iOS/ViewController.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var btnOpptAction: UIButton!
+    @IBOutlet weak var btnYouAction: UIButton!
+    @IBOutlet weak var btnBack: UIButton!
+    @IBOutlet weak var btnReset: UIButton!
+    @IBOutlet weak var btnSave: UIButton!
+    @IBOutlet weak var btnLoad: UIButton!
 
+    @IBOutlet weak var labelOpptSet: UILabel!
+    @IBOutlet weak var labelYouSet: UILabel!
+    @IBOutlet weak var labelOpptGame: UILabel!
+    @IBOutlet weak var labelYouGame: UILabel!
+    @IBOutlet weak var labelOpptPoint: UILabel!
+    @IBOutlet weak var labelYouPoint: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        btnOpptAction.setTitle(NSLocalizedString("game_action", comment: "Action"), for: UIControlState.normal)
+        
+        btnYouAction.setTitle(NSLocalizedString("game_action", comment: "Action"), for: UIControlState.normal)
+        
+        btnBack.setTitle(NSLocalizedString("game_back", comment: "Back"), for: UIControlState.normal)
+        
+        btnReset.setTitle(NSLocalizedString("game_reset", comment: "reset"), for: UIControlState.normal)
+        
+        btnSave.setTitle(NSLocalizedString("game_save", comment: "Save"), for: UIControlState.normal)
+        
+        btnLoad.setTitle(NSLocalizedString("game_load", comment: "Load"), for: UIControlState.normal)
+        
+        labelOpptSet.text = "0"
+        labelYouSet.text = "0"
+        labelOpptGame.text = "0"
+        labelYouGame.text = "0"
+        labelOpptPoint.text = "40A"
+        labelYouPoint.text = "40A"
+        
     }
 
     override func didReceiveMemoryWarning() {
