4c5d49f80815da7bb80ad6e3b1f30387c122e14e
SUNUP <sunup@sunupteki-mac-mini.local>
2017-11-01 16:08:03 +0800

    modify

--- TennisScoreBoard_iOS/TopMenu.swift
+++ TennisScoreBoard_iOS/TopMenu.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class TopMenu: UIViewController {
 
+    @IBOutlet weak var btnLoadGame: UIButton!
+    @IBOutlet weak var btnlNewGame: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        btnLoadGame.titleLabel?.numberOfLines = 1
+        btnLoadGame.titleLabel?.adjustsFontSizeToFitWidth = true
+        btnLoadGame.titleLabel?.lineBreakMode = NSLineBreakMode.byClipping
+        
+        btnlNewGame.titleLabel?.numberOfLines = 1
+        btnlNewGame.titleLabel?.adjustsFontSizeToFitWidth = true
+        btnlNewGame.titleLabel?.lineBreakMode = NSLineBreakMode.byClipping
     }
 
     override func didReceiveMemoryWarning() {
