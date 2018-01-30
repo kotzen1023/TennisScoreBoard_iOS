22bbe1a99a06bf09a5eed957b552a2c3d4958453
SUNUP <sunup@sunupteki-mac-mini.local>
2018-01-30 16:27:27 +0800

    modify in-app purchase

--- TennisScoreBoard_iOS/VoiceSelect.swift
+++ TennisScoreBoard_iOS/VoiceSelect.swift
@@ -9,6 +9,15 @@
 import UIKit
 import StoreKit
 
+extension SKProduct {
+    var localizedPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = priceLocale
+        return formatter.string(from: price)!
+    }
+}
+
 class VoiceSelect: UIViewController,UITableViewDelegate, UITableViewDataSource, SKProductsRequestDelegate, SKPaymentTransactionObserver {
     
     
@@ -19,9 +28,11 @@ class VoiceSelect: UIViewController,UITableViewDelegate, UITableViewDataSource, 
     
     //var UserDef:UserDefaults!
     var voice_select: NSInteger = 0
+    var voice_gbr_woman_purchased: Bool = false
     var voiceList: Array<String> = []
     var productIDs: Array<String> = []
     var productsArray: Array<SKProduct> = []
+    var purchasedArray: Array<Bool> = []
     
     var selectedProductIndex: Int!
     
@@ -40,12 +51,23 @@ class VoiceSelect: UIViewController,UITableViewDelegate, UITableViewDataSource, 
         //voice_select = UserDef.string(forKey: "VOICE_SELECT")!
         voice_select = UserDefaults.standard.integer(forKey: "VOICE_SELECT")
         
+        voice_gbr_woman_purchased = UserDefaults.standard.bool(forKey: "voice_support_gbr_woman")
+        
+        
         print("voice select = \(voice_select)")
         
         productIDs.removeAll()
-        productIDs.append("gbr_man_voice_support")
-        productIDs.append("gbr_woman_voice_support")
+        productIDs.append("voice_support_gbr_man")
+        productIDs.append("voice_support_gbr_woman")
 
+        
+        purchasedArray.removeAll()
+        purchasedArray.append(true)
+        if voice_gbr_woman_purchased {
+            purchasedArray.append(true)
+        } else {
+            purchasedArray.append(false)
+        }
         //voiceList.removeAll()
         //voiceList.append("GBR Man Voice")
         //voiceList.append("GBR Woman Voice")
@@ -55,8 +77,13 @@ class VoiceSelect: UIViewController,UITableViewDelegate, UITableViewDataSource, 
         
         requestProductInfo()
         
-        SKPaymentQueue.default().add(self)
+        //SKPaymentQueue.default().add(self)
         
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        // 移除觀查者
+        SKPaymentQueue.default().remove(self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -126,6 +153,10 @@ class VoiceSelect: UIViewController,UITableViewDelegate, UITableViewDataSource, 
         container.removeFromSuperview()
     }
     
+    func setInitValue() {
+        
+    }
+    
     func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
         let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
         let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
@@ -143,18 +174,32 @@ class VoiceSelect: UIViewController,UITableViewDelegate, UITableViewDataSource, 
                 withIdentifier: "voiceItemCell", for: indexPath) as
         UITableViewCell
         
-        let item: String = voiceList[indexPath.row]
+        let desc: String = voiceList[indexPath.row]
+        let price = productsArray[indexPath.row].localizedPrice
         let icon: UIImageView = cell.viewWithTag(100) as! UIImageView
         let title: UILabel = cell.viewWithTag(101) as! UILabel
+        let purchase: UILabel = cell.viewWithTag(102) as! UILabel
         
         if indexPath.row == 0 || indexPath.row == 1 {
             icon.image = UIImage(named: "ic_gbr_flag")!
             
         }
         
-        title.text = item as String
+        title.text = desc as String
+        if indexPath.row == 0 {
+            purchase.text = NSLocalizedString("voice_free", comment: "")
+        } else {
+            if purchasedArray[indexPath.row] {
+                purchase.text = NSLocalizedString("voice_purchased", comment: "")
+            } else {
+                purchase.text = price as String
+            }
+        }
         
-        print("cell[\(indexPath.row)] = \(item)" )
+        
+        
+        
+        print("cell[\(indexPath.row)] = \(desc), prize = \(productsArray[indexPath.row].localizedPrice)" )
         
         return cell
     }
@@ -166,17 +211,19 @@ class VoiceSelect: UIViewController,UITableViewDelegate, UITableViewDataSource, 
         //myUserDefaults.setValue(String(format:"%d", indexPath.row), forKey: "VOICE_SELECT")
         selectedProductIndex = indexPath.row
         
-        if (selectedProductIndex > 0) {
-            showActions()
+        if purchasedArray[indexPath.row] {
+            UserDefaults.standard.set(indexPath.row, forKey: "VOICE_SELECT")  //Integer
+            UserDefaults.standard.synchronize();
+            
+            NotificationCenter.default.post(name: Notification.Name("langChangeInfo"), object: nil)
+            
+            
+            print("set voice = \(indexPath.row)")
+        } else {
+            if (selectedProductIndex > 0) {
+                showActions()
+            }
         }
-        
-        UserDefaults.standard.set(indexPath.row, forKey: "VOICE_SELECT")  //Integer
-        UserDefaults.standard.synchronize();
-        
-        NotificationCenter.default.post(name: Notification.Name("langChangeInfo"), object: nil)
-
-        
-        print("set voice = \(indexPath.row)")
     }
     
     func showActions() {
@@ -193,11 +240,19 @@ class VoiceSelect: UIViewController,UITableViewDelegate, UITableViewDataSource, 
             
             print("select: \(self.selectedProductIndex)")
             
-            if self.productsArray.count > 0 {
-                let payment = SKPayment(product: self.productsArray[self.selectedProductIndex] as SKProduct)
-                SKPaymentQueue.default().add(payment)
-                self.transactionInProgress = true
+            if SKPaymentQueue.canMakePayments() {
+                SKPaymentQueue.default().add(self)
+                
+                if self.productsArray.count > 0 {
+                    let payment = SKPayment(product: self.productsArray[self.selectedProductIndex] as SKProduct)
+                    SKPaymentQueue.default().add(payment)
+                    self.transactionInProgress = true
+                    
+                    self.showActivityIndicator(uiView: self.view)
+                }
             }
+            
+            
         }
         
         let cancelAction = UIAlertAction(title: NSLocalizedString("voice_cancel", comment: ""), style: UIAlertActionStyle.cancel) { (action) -> Void in
@@ -237,7 +292,7 @@ class VoiceSelect: UIViewController,UITableViewDelegate, UITableViewDataSource, 
             voiceList.removeAll();
             var count: NSInteger = 0
             for product in response.products {
-                print("productsArray[\(count)]=\(product.localizedDescription)")
+                print("productsArray[\(count)]=\(product.localizedDescription), purchased=\(product.price)")
                 productsArray.append(product )
                 voiceList.append(product.localizedDescription)
                 count = count + 1
@@ -259,11 +314,35 @@ class VoiceSelect: UIViewController,UITableViewDelegate, UITableViewDataSource, 
                 print("Transaction completed successfully.")
                 SKPaymentQueue.default().finishTransaction(transaction)
                 transactionInProgress = false
+                
+                SKPaymentQueue.default().remove(self)
+                
                 didBuyColorsCollection(collectionIndex: selectedProductIndex)
                 
+                hideActivityIndicator(uiView: self.view)
+                
+                UserDefaults.standard.set(true, forKey: productIDs[self.selectedProductIndex])
+                UserDefaults.standard.synchronize();
                 
             case SKPaymentTransactionState.failed:
                 print("Transaction Failed");
+                
+                if let error = transaction.error as? SKError {
+                    switch error.code {
+                    case .paymentCancelled:
+                        // 輸入 Apple ID 密碼時取消
+                        print("Transaction Cancelled: \(error.localizedDescription)")
+                    case .paymentInvalid:
+                        print("Transaction paymentInvalid: \(error.localizedDescription)")
+                    case .paymentNotAllowed:
+                        print("Transaction paymentNotAllowed: \(error.localizedDescription)")
+                    default:
+                        print("Transaction: \(error.localizedDescription)")
+                    }
+                }
+                
+                hideActivityIndicator(uiView: self.view)
+                
                 SKPaymentQueue.default().finishTransaction(transaction)
                 transactionInProgress = false
                 
@@ -283,4 +362,42 @@ class VoiceSelect: UIViewController,UITableViewDelegate, UITableViewDataSource, 
             //btnGray.hidden = false
         }
     }
+    
+    // 提示購買或回復商品的訊息
+    func showMessage(_ product: VoiceProduct) {
+        var message: String!
+        
+        switch product {
+        case .voice_support_gbr_man:
+            message = "buy voice_support_gbr_man success！"
+        case .voice_support_gbr_woman:
+            message = "buy voice_support_gbr_woman success！"
+        case .restore:
+            message = "回復成功！"
+        }
+        
+        let alertController = UIAlertController(title: "提示", message: message, preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "是", style: .default, handler: nil)
+        
+        alertController.addAction(confirm)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    // 復原購買失敗
+    func paymentQueue(_ queue: SKPaymentQueue, restoreCompletedTransactionsFailedWithError error: Error) {
+        print("復原購買失敗...")
+        print(error.localizedDescription)
+    }
+    
+    // 回復購買成功(若沒實作該 delegate 會有問題產生)
+    func paymentQueueRestoreCompletedTransactionsFinished(_ queue: SKPaymentQueue) {
+        print("復原購買成功...")
+    }
+}
+
+enum VoiceProduct {
+    case voice_support_gbr_man
+    case voice_support_gbr_woman
+    case restore
 }
